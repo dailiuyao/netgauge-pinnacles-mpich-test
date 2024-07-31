b03998427b9014735a20f49f266601fb835331e7
Liuyao Dai <ldai8@polaris-login-02.hsn.cm.polaris.alcf.anl.gov>
2024-07-31 22:02:20 +0000

    small message on polaris

--- ncclguage/frequency_test/padsys/Get_Frequency_Run.sh
+++ ncclguage/frequency_test/padsys/Get_Frequency_Run.sh
@@ -9,5 +9,3 @@ export NVCC_GENCODE="-gencode=arch=compute_80,code=sm_80"
 ./GetCudaFrequency.exe
 
 
-# GPU Clock Rate: 1815000 kHz
-# GPU Clock Rate: 1815000064 Hz
--- ncclguage/polaris/gauge-run.sh
+++ ncclguage/polaris/gauge-run.sh
@@ -54,12 +54,12 @@ export GAUGE_OUT_DIRE="$NCCL_GAUGE_HOME/polaris"
 export GAUGE_HEO="inter"
 export GAUGE_CHUNK_SIZE="2"
 
-export ITERATION_TIME="2"
+export ITERATION_TIME="30"
 
 export COMM_GPU_ID="0"
 
-export GAUGE_MIN_NTHREADS=256
-export GAUGE_MAX_NTHREADS=256
+export GAUGE_MIN_NTHREADS=64
+export GAUGE_MAX_NTHREADS=64
 
 export GAUGE_MIN_NCHANNELS=1
 export GAUGE_MAX_NCHANNELS=1
@@ -69,7 +69,7 @@ export GAUGE_MAX_NCHANNELS=1
 
 # /home1/09168/ldai1/bin/dool --time --mem --cpu --net -N eno1,ib0,lo,total 1 > $NCCL_GAUGE_HOME/frontera/dool.csv &
 
-for ((itr = 0; itr < ${ITERATION_TIME}; itr += 1)); do
+for ((itr = 10; itr < ${ITERATION_TIME}; itr += 1)); do
 
     # NCCL source location
     NCCL_SRC_LOCATION="/home/ldai8/ccl/NCCL_profile"
@@ -94,22 +94,22 @@ for ((itr = 0; itr < ${ITERATION_TIME}; itr += 1)); do
                             export NCCL_NTHREADS=${nth}
                             export GAUGE_STEP_SIZE="0"
                             $MPIEXEC_HOME/bin/mpirun -n 2 --ppn 1 --cpu-bind core $NCCL_GAUGE_HOME/gauge/${mode}_gauge_n_${n}_${sync_mode}_d_${d}.exe
-                            # export GAUGE_STEP_SIZE="32"
-                            # for ((msize=(${GAUGE_STEP_SIZE}/8); msize<4*${GAUGE_STEP_SIZE}; msize+=(${GAUGE_STEP_SIZE}/8))); do
-                            #     export GAUGE_MESSAGE_SIZE=${msize}
-                            #     $MPIEXEC_HOME/bin/mpirun -n 2 --ppn 1 --cpu-bind core $NCCL_GAUGE_HOME/gauge/${mode}_gauge_n_${n}_${sync_mode}_d_${d}.exe
-                            #     # ibrun -n 2 --ntasks-per-node=2 \
-                            #     # bash -c "nsys profile --force-overwrite true -o p2p_profile_d_0_n_${n}_${mode}_%q{SLURM_PROCID} --trace=cuda,nvtx,osrt --stats=true $NCCL_GAUGE_HOME/gauge/${mode}_gauge_${n}.exe"
-                            #     # ibrun -n 2 --ntasks-per-node=2 ncu --mode=launch $NCCL_GAUGE_HOME/gauge/${mode}_gauge_${n}.exe
-                            # done
-                            # export GAUGE_STEP_SIZE="64"
-                            # for ((msize=(${GAUGE_STEP_SIZE}*2); msize<16*${GAUGE_STEP_SIZE}; msize+=${GAUGE_STEP_SIZE})); do
-                            #     export GAUGE_MESSAGE_SIZE=${msize}
-                            #     $MPIEXEC_HOME/bin/mpirun -n 2 --ppn 1 --cpu-bind core $NCCL_GAUGE_HOME/gauge/${mode}_gauge_n_${n}_${sync_mode}_d_${d}.exe
-                            #     # ibrun -n 2 --ntasks-per-node=2 \
-                            #     # bash -c "nsys profile --force-overwrite true -o p2p_profile_d_0_n_${n}_${mode}_%q{SLURM_PROCID} --trace=cuda,nvtx,osrt --stats=true $NCCL_GAUGE_HOME/gauge/${mode}_gauge_${n}.exe"
-                            #     # ibrun -n 2 --ntasks-per-node=2 ncu --mode=launch $NCCL_GAUGE_HOME/gauge/${mode}_gauge_${n}.exe
-                            # done
+                            export GAUGE_STEP_SIZE="32"
+                            for ((msize=(${GAUGE_STEP_SIZE}/8); msize<4*${GAUGE_STEP_SIZE}; msize+=(${GAUGE_STEP_SIZE}/8))); do
+                                export GAUGE_MESSAGE_SIZE=${msize}
+                                $MPIEXEC_HOME/bin/mpirun -n 2 --ppn 1 --cpu-bind core $NCCL_GAUGE_HOME/gauge/${mode}_gauge_n_${n}_${sync_mode}_d_${d}.exe
+                                # ibrun -n 2 --ntasks-per-node=2 \
+                                # bash -c "nsys profile --force-overwrite true -o p2p_profile_d_0_n_${n}_${mode}_%q{SLURM_PROCID} --trace=cuda,nvtx,osrt --stats=true $NCCL_GAUGE_HOME/gauge/${mode}_gauge_${n}.exe"
+                                # ibrun -n 2 --ntasks-per-node=2 ncu --mode=launch $NCCL_GAUGE_HOME/gauge/${mode}_gauge_${n}.exe
+                            done
+                            export GAUGE_STEP_SIZE="64"
+                            for ((msize=(${GAUGE_STEP_SIZE}*2); msize<16*${GAUGE_STEP_SIZE}; msize+=${GAUGE_STEP_SIZE})); do
+                                export GAUGE_MESSAGE_SIZE=${msize}
+                                $MPIEXEC_HOME/bin/mpirun -n 2 --ppn 1 --cpu-bind core $NCCL_GAUGE_HOME/gauge/${mode}_gauge_n_${n}_${sync_mode}_d_${d}.exe
+                                # ibrun -n 2 --ntasks-per-node=2 \
+                                # bash -c "nsys profile --force-overwrite true -o p2p_profile_d_0_n_${n}_${mode}_%q{SLURM_PROCID} --trace=cuda,nvtx,osrt --stats=true $NCCL_GAUGE_HOME/gauge/${mode}_gauge_${n}.exe"
+                                # ibrun -n 2 --ntasks-per-node=2 ncu --mode=launch $NCCL_GAUGE_HOME/gauge/${mode}_gauge_${n}.exe
+                            done
                             # export GAUGE_STEP_SIZE="128"
                             # for ((msize=(${GAUGE_STEP_SIZE}*8); msize<=128*${GAUGE_STEP_SIZE}; msize+=${GAUGE_STEP_SIZE})); do
                             #     export GAUGE_MESSAGE_SIZE=${msize}
@@ -118,14 +118,14 @@ for ((itr = 0; itr < ${ITERATION_TIME}; itr += 1)); do
                             #     # bash -c "nsys profile --force-overwrite true -o p2p_profile_d_0_n_${n}_${mode}_%q{SLURM_PROCID} --trace=cuda,nvtx,osrt --stats=true $NCCL_GAUGE_HOME/gauge/${mode}_gauge_${n}.exe"
                             #     # ibrun -n 2 --ntasks-per-node=2 ncu --mode=launch $NCCL_GAUGE_HOME/gauge/${mode}_gauge_${n}.exe
                             # done
-                            export GAUGE_STEP_SIZE="1048576"
-                            for ((msize=(${GAUGE_STEP_SIZE}); msize<=16*${GAUGE_STEP_SIZE}; msize+=${GAUGE_STEP_SIZE})); do
-                                export GAUGE_MESSAGE_SIZE=${msize}
-                                $MPIEXEC_HOME/bin/mpirun -n 2 --ppn 1 --cpu-bind core $NCCL_GAUGE_HOME/gauge/${mode}_gauge_n_${n}_${sync_mode}_d_${d}.exe
-                                # ibrun -n 2 --ntasks-per-node=2 \
-                                # bash -c "nsys profile --force-overwrite true -o p2p_profile_d_0_n_${n}_${mode}_%q{SLURM_PROCID} --trace=cuda,nvtx,osrt --stats=true $NCCL_GAUGE_HOME/gauge/${mode}_gauge_${n}.exe"
-                                # ibrun -n 2 --ntasks-per-node=2 ncu --mode=launch $NCCL_GAUGE_HOME/gauge/${mode}_gauge_${n}.exe
-                            done
+                            # export GAUGE_STEP_SIZE="1048576"
+                            # for ((msize=(${GAUGE_STEP_SIZE}); msize<=16*${GAUGE_STEP_SIZE}; msize+=${GAUGE_STEP_SIZE})); do
+                            #     export GAUGE_MESSAGE_SIZE=${msize}
+                            #     $MPIEXEC_HOME/bin/mpirun -n 2 --ppn 1 --cpu-bind core $NCCL_GAUGE_HOME/gauge/${mode}_gauge_n_${n}_${sync_mode}_d_${d}.exe
+                            #     # ibrun -n 2 --ntasks-per-node=2 \
+                            #     # bash -c "nsys profile --force-overwrite true -o p2p_profile_d_0_n_${n}_${mode}_%q{SLURM_PROCID} --trace=cuda,nvtx,osrt --stats=true $NCCL_GAUGE_HOME/gauge/${mode}_gauge_${n}.exe"
+                            #     # ibrun -n 2 --ntasks-per-node=2 ncu --mode=launch $NCCL_GAUGE_HOME/gauge/${mode}_gauge_${n}.exe
+                            # done
                         done
                     done
                 done
@@ -135,59 +135,59 @@ for ((itr = 0; itr < ${ITERATION_TIME}; itr += 1)); do
 
 
 
-    # # NCCL source location
-    # NCCL_SRC_LOCATION="/home/ldai8/ccl/NCCL_profile_D"
+    # NCCL source location
+    NCCL_SRC_LOCATION="/home/ldai8/ccl/NCCL_profile_D"
 
-    # # Update to include the correct path for NVCC and MPI library paths
-    # export PATH=${CUDA_HOME}/bin:${MPI_HOME}/bin:${PATH}
-    # export LD_LIBRARY_PATH=${NCCL_SRC_LOCATION}/build/lib:${MPI_HOME}/lib:${CUDA_HOME}/lib64:${LD_LIBRARY_PATH}
+    # Update to include the correct path for NVCC and MPI library paths
+    export PATH=${CUDA_HOME}/bin:${MPI_HOME}/bin:${PATH}
+    export LD_LIBRARY_PATH=${NCCL_SRC_LOCATION}/build/lib:${MPI_HOME}/lib:${CUDA_HOME}/lib64:${LD_LIBRARY_PATH}
 
-    # # for sync_mode in sync group; do
-    # for sync_mode in sync; do
-    #     for ((n = 1; n <= 1; n *= 8)); do
-    #         for ((nch = ${GAUGE_MIN_NCHANNELS}; nch <= ${GAUGE_MAX_NCHANNELS}; nch *= 2)); do
-    #             for mode in pping; do
-    #                 for ((nth = ${GAUGE_MIN_NTHREADS}; nth <= ${GAUGE_MAX_NTHREADS}; nth *= 2)); do
-    #                     for ((d = 2000; d <= 2000; d += 2000)); do
-    #                         export GAUGE_ITERATION=${itr} 
-    #                         export GAUGE_NCHANNELS=${nch}
-    #                         export GAUGE_MODE=${mode}
-    #                         export NCCL_MIN_NCHANNELS=${nch}
-    #                         export NCCL_MAX_NCHANNELS=${nch}
-    #                         export GAUGE_MESSAGE_SIZE=1
-    #                         export NCCL_NTHREADS=${nth}
-    #                         export GAUGE_STEP_SIZE="1000"
-    #                         $MPIEXEC_HOME/bin/mpirun -n 2 --ppn 1 --cpu-bind core $NCCL_GAUGE_HOME/gauge/${mode}_gauge_n_${n}_${sync_mode}_d_${d}.exe
-    #                         export GAUGE_STEP_SIZE="32"
-    #                         for ((msize=(${GAUGE_STEP_SIZE}/8); msize<4*${GAUGE_STEP_SIZE}; msize+=(${GAUGE_STEP_SIZE}/8))); do
-    #                             export GAUGE_MESSAGE_SIZE=${msize}
-    #                             $MPIEXEC_HOME/bin/mpirun -n 2 --ppn 1 --cpu-bind core $NCCL_GAUGE_HOME/gauge/${mode}_gauge_n_${n}_${sync_mode}_d_${d}.exe
-    #                             # ibrun -n 2 --ntasks-per-node=2 \
-    #                             # bash -c "nsys profile --force-overwrite true -o p2p_profile_d_0_n_${n}_${mode}_%q{SLURM_PROCID} --trace=cuda,nvtx,osrt --stats=true $NCCL_GAUGE_HOME/gauge/${mode}_gauge_${n}.exe"
-    #                             # ibrun -n 2 --ntasks-per-node=2 ncu --mode=launch $NCCL_GAUGE_HOME/gauge/${mode}_gauge_${n}.exe
-    #                         done
-    #                         export GAUGE_STEP_SIZE="64"
-    #                         for ((msize=(${GAUGE_STEP_SIZE}*2); msize<16*${GAUGE_STEP_SIZE}; msize+=${GAUGE_STEP_SIZE})); do
-    #                             export GAUGE_MESSAGE_SIZE=${msize}
-    #                             $MPIEXEC_HOME/bin/mpirun -n 2 --ppn 1 --cpu-bind core $NCCL_GAUGE_HOME/gauge/${mode}_gauge_n_${n}_${sync_mode}_d_${d}.exe
-    #                             # ibrun -n 2 --ntasks-per-node=2 \
-    #                             # bash -c "nsys profile --force-overwrite true -o p2p_profile_d_0_n_${n}_${mode}_%q{SLURM_PROCID} --trace=cuda,nvtx,osrt --stats=true $NCCL_GAUGE_HOME/gauge/${mode}_gauge_${n}.exe"
-    #                             # ibrun -n 2 --ntasks-per-node=2 ncu --mode=launch $NCCL_GAUGE_HOME/gauge/${mode}_gauge_${n}.exe
-    #                         done
-    #                         export GAUGE_STEP_SIZE="128"
-    #                         for ((msize=(${GAUGE_STEP_SIZE}*8); msize<=128*${GAUGE_STEP_SIZE}; msize+=${GAUGE_STEP_SIZE})); do
-    #                             export GAUGE_MESSAGE_SIZE=${msize}
-    #                             $MPIEXEC_HOME/bin/mpirun -n 2 --ppn 1 --cpu-bind core $NCCL_GAUGE_HOME/gauge/${mode}_gauge_n_${n}_${sync_mode}_d_${d}.exe
-    #                             # ibrun -n 2 --ntasks-per-node=2 \
-    #                             # bash -c "nsys profile --force-overwrite true -o p2p_profile_d_0_n_${n}_${mode}_%q{SLURM_PROCID} --trace=cuda,nvtx,osrt --stats=true $NCCL_GAUGE_HOME/gauge/${mode}_gauge_${n}.exe"
-    #                             # ibrun -n 2 --ntasks-per-node=2 ncu --mode=launch $NCCL_GAUGE_HOME/gauge/${mode}_gauge_${n}.exe
-    #                         done
-    #                     done
-    #                 done
-    #             done
-    #         done 
-    #     done
-    # done
+    # for sync_mode in sync group; do
+    for sync_mode in sync; do
+        for ((n = 1; n <= 1; n *= 8)); do
+            for ((nch = ${GAUGE_MIN_NCHANNELS}; nch <= ${GAUGE_MAX_NCHANNELS}; nch *= 2)); do
+                for mode in pping; do
+                    for ((nth = ${GAUGE_MIN_NTHREADS}; nth <= ${GAUGE_MAX_NTHREADS}; nth *= 2)); do
+                        for ((d = 2000; d <= 2000; d += 2000)); do
+                            export GAUGE_ITERATION=${itr} 
+                            export GAUGE_NCHANNELS=${nch}
+                            export GAUGE_MODE=${mode}
+                            export NCCL_MIN_NCHANNELS=${nch}
+                            export NCCL_MAX_NCHANNELS=${nch}
+                            export GAUGE_MESSAGE_SIZE=1
+                            export NCCL_NTHREADS=${nth}
+                            export GAUGE_STEP_SIZE="1000"
+                            $MPIEXEC_HOME/bin/mpirun -n 2 --ppn 1 --cpu-bind core $NCCL_GAUGE_HOME/gauge/${mode}_gauge_n_${n}_${sync_mode}_d_${d}.exe
+                            export GAUGE_STEP_SIZE="32"
+                            for ((msize=(${GAUGE_STEP_SIZE}/8); msize<4*${GAUGE_STEP_SIZE}; msize+=(${GAUGE_STEP_SIZE}/8))); do
+                                export GAUGE_MESSAGE_SIZE=${msize}
+                                $MPIEXEC_HOME/bin/mpirun -n 2 --ppn 1 --cpu-bind core $NCCL_GAUGE_HOME/gauge/${mode}_gauge_n_${n}_${sync_mode}_d_${d}.exe
+                                # ibrun -n 2 --ntasks-per-node=2 \
+                                # bash -c "nsys profile --force-overwrite true -o p2p_profile_d_0_n_${n}_${mode}_%q{SLURM_PROCID} --trace=cuda,nvtx,osrt --stats=true $NCCL_GAUGE_HOME/gauge/${mode}_gauge_${n}.exe"
+                                # ibrun -n 2 --ntasks-per-node=2 ncu --mode=launch $NCCL_GAUGE_HOME/gauge/${mode}_gauge_${n}.exe
+                            done
+                            export GAUGE_STEP_SIZE="64"
+                            for ((msize=(${GAUGE_STEP_SIZE}*2); msize<16*${GAUGE_STEP_SIZE}; msize+=${GAUGE_STEP_SIZE})); do
+                                export GAUGE_MESSAGE_SIZE=${msize}
+                                $MPIEXEC_HOME/bin/mpirun -n 2 --ppn 1 --cpu-bind core $NCCL_GAUGE_HOME/gauge/${mode}_gauge_n_${n}_${sync_mode}_d_${d}.exe
+                                # ibrun -n 2 --ntasks-per-node=2 \
+                                # bash -c "nsys profile --force-overwrite true -o p2p_profile_d_0_n_${n}_${mode}_%q{SLURM_PROCID} --trace=cuda,nvtx,osrt --stats=true $NCCL_GAUGE_HOME/gauge/${mode}_gauge_${n}.exe"
+                                # ibrun -n 2 --ntasks-per-node=2 ncu --mode=launch $NCCL_GAUGE_HOME/gauge/${mode}_gauge_${n}.exe
+                            done
+                            # export GAUGE_STEP_SIZE="128"
+                            # for ((msize=(${GAUGE_STEP_SIZE}*8); msize<=128*${GAUGE_STEP_SIZE}; msize+=${GAUGE_STEP_SIZE})); do
+                            #     export GAUGE_MESSAGE_SIZE=${msize}
+                            #     $MPIEXEC_HOME/bin/mpirun -n 2 --ppn 1 --cpu-bind core $NCCL_GAUGE_HOME/gauge/${mode}_gauge_n_${n}_${sync_mode}_d_${d}.exe
+                            #     # ibrun -n 2 --ntasks-per-node=2 \
+                            #     # bash -c "nsys profile --force-overwrite true -o p2p_profile_d_0_n_${n}_${mode}_%q{SLURM_PROCID} --trace=cuda,nvtx,osrt --stats=true $NCCL_GAUGE_HOME/gauge/${mode}_gauge_${n}.exe"
+                            #     # ibrun -n 2 --ntasks-per-node=2 ncu --mode=launch $NCCL_GAUGE_HOME/gauge/${mode}_gauge_${n}.exe
+                            # done
+                        done
+                    done
+                done
+            done 
+        done
+    done
 
 done
 
